78921951dc9ef4b7a48af7cbea6ab73cf811b2f4
x <x@xs-MacBook-Pro.local>
2020-03-10 16:06:49 -0500

    Updates

---
+++ precache-manifest.9a91661fe03b8bcbf7d0b308402070f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94b15423a9e21ac5ad62",
+    "url": "/seasons_hooks/static/js/runtime~main.94b15423.js"
+  },
+  {
+    "revision": "828154d68b702bb034d0",
+    "url": "/seasons_hooks/static/js/main.828154d6.chunk.js"
+  },
+  {
+    "revision": "78fa1d7f359a8e9073d8",
+    "url": "/seasons_hooks/static/js/1.78fa1d7f.chunk.js"
+  },
+  {
+    "revision": "828154d68b702bb034d0",
+    "url": "/seasons_hooks/static/css/main.e9c36816.chunk.css"
+  },
+  {
+    "revision": "c44f8275d04fe8ed77a0f6955c0104a5",
+    "url": "/seasons_hooks/index.html"
+  }
+];
